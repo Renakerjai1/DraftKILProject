e25b0c6cb524dee872b17d542ec796a3f1c334e9
Renakerjai1 <65046019+Renakerjai1@users.noreply.github.com>
2022-06-26 17:55:23 -0400

    added some more rough parts and blueprinting a bit

--- GUI.py
+++ GUI.py
@@ -1,9 +1,19 @@
 import tkinter as tk
-window =tk.Tk()
-window.geometry("640x600")
-greeting=tk.Label(text="10 second ads",foreground="white",background="black")
-like=tk.Button(
-    text="like",width="5",height="2",bg="red",fg="white"
-)
-entry=tk.Entry(fg="white",bg="blue",width=50)
-greeting.pack(),entry.pack(),like.pack()
+import PIL
+from PIL import ImageTk,Image
+class AppWindow:
+    """
+    make product a tuple or a list and make a recycler style view that shows a certain number of images, less than 10,
+    with one or two videos
+
+    need to find a way to create a revolving list of products that preview photos and videos
+
+    """
+    def __init__(self,product):
+        appwindow=tk.Tk()
+        appwindow.title('Ten second ads')
+        appwindow.geometry("640x600")
+        like=tk.Button(text="like",width="5",height="2",bg="red",fg="white")
+        entry=tk.Entry(fg="white",bg="blue",width=50)
+        img=ImageTk.PhotoImage(product.photo)
+        entry.pack(),like.pack()
--- product.py
+++ product.py
@@ -1,6 +1,23 @@
 import json
 """
-each product should have a title, a tag, a 10 second video and at least two pictures or maybe none.
-
+each product should have a title, a tag, a 10 second video and optionally pictures
+maybe add a photo by a 
 
 """
+class product:
+    def __init__(self,video):
+        self.video=video
+        self.pictures=[]
+
+    def addPhoto(self,photo):
+        """
+        need to check to make sure a photo is a singular photo and not a group or
+        :param photo: a singular photo
+        :return: a success message or a failure message
+        """
+        if (len(self.pictures)>=10):
+            return "you must first delete a picture to add a new one"
+        else:
+            self.pictures.append(photo)
+            return "photo successfully added"
+
